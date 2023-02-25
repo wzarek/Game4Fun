4231fc68bd0f82d3a7647a7fc93ff6a37a3b46b2
bilimig <126111610+bilimig@users.noreply.github.com>
2023-02-25 20:22:39 +0100

    Kamera

--- Assets/Scipts/ControlCamera.cs
+++ Assets/Scipts/ControlCamera.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,31 @@ using UnityEngine;
 public class ControlCamera : MonoBehaviour
 {
 
-    public float PlayerSpeed = 2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject _zoomCamera;
+    private GameObject _normalCamera;
+
 
     // Update is called once per frame
     void Update()
     {
 
+        if(Input.GetButtonDown("Fire2"))
+        {
+          
+            _zoomCamera.GetComponent<CinemachineFreeLook>().Priority = 11;
+        }
+        if(Input.GetButtonUp("Fire2"))
+        {
+          
+            _zoomCamera.GetComponent<CinemachineFreeLook>().Priority = 9;
+           
+        }
+    }
+
+    private void Awake()
+    {
+        _zoomCamera = GameObject.Find("Player/Camera/ThirdPersonCameraZoom");
+        _normalCamera = GameObject.Find("Player/Camera/ThirdPersonCamera");
 
     }
 }
